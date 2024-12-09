8b86b2e6449a7b809d269951c5c3b51b979fe262
root <gideonmatabologa@gmail.com>
2024-12-09 12:26:26 -0800

    Task_1

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+number_1 = 10
+number_2 = 5
+
+def addition():
+    added = number_1 + number_2
+    print(f'Addition of 10 and 5 is {added}')
+
+def subtraction():
+    subtract = number_1 - number_2
+    print(f'subtraction of 10 and 5 is {subtract}')
+
+def multiplication():
+    multiply = number_1 + number_2
+    print(f'multiplication of 10 and 5 is {multiply}')
+
+addition()
+
+subtraction()
+
+multiplication()
